9018e5f0d421bf601abffcf1ec3ae1b80eaf0cba
Mehdi <mehdi.mana@manaitservices.com>
2018-03-05 17:27:23 +0100

    add config

--- truffle.js
+++ truffle.js
@@ -3,7 +3,7 @@ module.exports = {
     development: {
       host: "localhost",
       port: 9545,
-      gas: 500000, // We know this is enough for this project, not necessarily your future projects
+      gas: 500000, 
       network_id: "*" // Match any network id
     }
   }
