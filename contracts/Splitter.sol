bc85fcd4120c0eca9657eb3eecc261cb3efec9a7
Mehdi <mehdi.mana@manaitservices.com>
2018-03-08 12:52:18 +0100

    don forget the code

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.19;
 contract Splitter {
     struct Receiver {
         address owner;
-        uint ammountAlreadyReceived;
+        uint ammountAvailable;
     }
 
     address public owner;
@@ -15,25 +15,39 @@ contract Splitter {
         owner = msg.sender;
         
         require(receiver1 != address(0));
-        require(receiver1 != owner);
+        require(receiver1 != msg.sender);
         require(receiver2 != address(0));
-        require(receiver2 != owner);
-        
+        require(receiver2 != msg.sender);
         
         firstReceiver.owner = receiver1;
-        firstReceiver.ammountAlreadyReceived = 0;
-        
         secondReceiver.owner = receiver2;
-        secondReceiver.ammountAlreadyReceived = 0;
         
         isContractActive = true;
     }
     
     /**
+     * a receiver withdraw his available balance
+     */
+    function withdrawFunds() public {
+        uint ammountToTransfer;
+        if (msg.sender == firstReceiver.owner) {
+            ammountToTransfer = firstReceiver.ammountAvailable;
+            firstReceiver.ammountAvailable = 0;
+            firstReceiver.owner.transfer(ammountToTransfer);
+        } else if (msg.sender == secondReceiver.owner) {
+            ammountToTransfer = secondReceiver.ammountAvailable;
+            secondReceiver.ammountAvailable = 0;
+            secondReceiver.owner.transfer(ammountToTransfer);
+        } else {
+            assert(false);
+        }
+    }
+
+    /**
      * owner sends funcds to this contract
      * - only owner can send funds
      * - ammount sent cannot be 0
-     * - amount is split between receivers.
+     * - amount is split between receivers but not ssent to the them
      */
     function sendFunds() public payable returns(bool success) {
         require(msg.sender == owner);
@@ -49,20 +63,11 @@ contract Splitter {
      * returns the total ammount that is send to the receivers;
      */
     function split(uint ammountToSplit) private {
-        uint ammountToSend = ammountToSplit /2;
-        firstReceiver = splitTo(firstReceiver, ammountToSend);
-        secondReceiver = splitTo(secondReceiver, ammountToSend);
+        uint ammountReceived = ammountToSplit /2;
+        firstReceiver.ammountAvailable += ammountReceived;
+        secondReceiver.ammountAvailable += ammountReceived;
     }
-    
-    /**
-     * sends funds to one receiver
-     */
-    function splitTo(Receiver receiver, uint ammountToSend) private returns(Receiver){
-        receiver.owner.transfer(ammountToSend);
-        receiver.ammountAlreadyReceived += ammountToSend;
-        return receiver;
-    }
-    
+
     /**
      * kill switch
      */
